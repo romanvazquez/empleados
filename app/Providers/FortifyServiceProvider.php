22ad8a6326fcb0fee642e597339cf2cf28abfc76
Román Vázquez <rdjvazqueza@gmail.com>
2023-05-04 09:01:00 -0600

    Recursos de Laravel Fortify

---
+++ app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Fortify\CreateNewUser;
+// use App\Actions\Fortify\ResetUserPassword;
+// use App\Actions\Fortify\UpdateUserPassword;
+// use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+
+        Fortify::registerView(function () {
+            return view('auth.register');
+        });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::whereClave($request->clave)->firstOr(function () {
+                throw ValidationException::withMessages([
+                    Fortify::username() => 'El usuario ingresado no existe.',
+                ]);
+            });
+
+            if(Hash::check($request->password, $user->password)){
+                return $user;
+            }
+        });
+
+        Fortify::createUsersUsing(CreateNewUser::class);
+        // Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        // Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        // Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        RateLimiter::for('login', function (Request $request) {
+            $clave = (string) $request->clave;
+
+            return Limit::perMinute(5)->by($clave.$request->ip());
+        });
+
+        // RateLimiter::for('two-factor', function (Request $request) {
+        //     return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        // });
+    }
+}
