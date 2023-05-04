ce43dc03b056f7a1a97d01a4b373317225b141c7
Román Vázquez <rdjvazqueza@gmail.com>
2023-05-04 10:07:32 -0600

    Menú

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -16,4 +16,29 @@
             </span>
         </a>
     </li>
+    <li class="nav-item dropdown {{ request()->routeIs('usuarios') || request()->routeIs('empleados') ? 'active' : '' }}">
+        <a class="nav-link dropdown-toggle" href="#navbar-help" data-bs-toggle="dropdown" data-bs-auto-close="off" role="button">
+            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <circle cx="9" cy="7" r="4"></circle>
+                    <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                    <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                    <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
+                </svg>
+            </span>
+            <span class="nav-link-title">
+                Empleados
+            </span>
+        </a>
+        <div class="dropdown-menu {{
+            $is_vertical &&
+            (
+                request()->routeIs('usuarios') ||
+                request()->routeIs('empleados')
+            ) ? 'show' : '' }}" data-bs-popper="static">
+            <a class="dropdown-item {{ request()->routeIs('empleados') ? 'active' : '' }}" href="{{ route('empleados') }}">Empleados</a>
+            <a class="dropdown-item {{ request()->routeIs('usuarios') ? 'active' : '' }}" href="{{ route('usuarios') }}">Usuarios</a>
+        </div>
+    </li>
 </ul>
